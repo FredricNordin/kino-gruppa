d0a6ccbd03a3d24edd2594ccd2f7fd9bb40a9f04
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-01-31 15:57:28 +0100

    added meta data to new filtered reviews obj

--- src/js/modules/reviews/movieReviewFilter.js
+++ src/js/modules/reviews/movieReviewFilter.js
@@ -2,27 +2,28 @@ import { fetchMovieReviews } from "./fetchReviews.js";
 
 export default async function reviewPageFilter(req, res) {
   const allReviews = await fetchMovieReviews(req.params.movieId);
-  if (Object.keys(req.query).length === 0) return res.send(allReviews.data);
+  if (Object.keys(req.query).length === 0) return res.send(allReviews);
 
   //filter logic
   const pageNr = req.query.page;
   const pageLimit = 5;
-  const pageStart = (pageNr - 1) * pageLimit;
+  const pageStart = (pageNr - 1) * pageLimit; //- 1 to get index with base 0
   const pageEnd = pageStart + pageLimit;
 
   //filtration
-  let limitedReviews = [];
+  let filteredReviews = { data: [], meta: {} };
   allReviews.data.forEach((review) => {
     if (
       allReviews.data.indexOf(review) >= pageStart &&
       allReviews.data.indexOf(review) < pageEnd
     ) {
-      limitedReviews.push(review);
+      filteredReviews.data.push(review);
     }
   });
 
-  console.log({ totalPages: Math.ceil(allReviews.data.length / pageLimit) });
-  console.log(limitedReviews);
+  filteredReviews.meta = {
+    totalPages: Math.ceil(allReviews.data.length / pageLimit),
+  };
 
-  res.status(200).send(limitedReviews);
+  res.status(200).send(filteredReviews);
 }
