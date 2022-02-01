0042a56ebb8f5e90addb799d69b5bb0d315b2cbc
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-02-01 14:40:13 +0100

    Add fetch for screenings from r-api, add filtering of screenings of single movies

--- src/js/modules/fetchData.js
+++ src/js/modules/fetchData.js
@@ -15,3 +15,26 @@ export async function fetchMovie(id) {
   const data = await response.json();
   return data.data;
 }
+
+export async function fetchScreenings(qs) {
+  const response = await fetch(
+    `https://lernia-kino-cms.herokuapp.com/api/screenings${qs}`
+  );
+  const data = await response.json();
+  return data.data;
+}
+
+export async function filterSingleScreenings(id) {
+  const today = new Date().toISOString();
+
+  const obj = await fetchScreenings(`?filters[movie]=${id}`);
+  const filteredScreenings = [];
+
+  obj.forEach((obj) => {
+    if (obj.attributes.start_time >= today) filteredScreenings.push(obj);
+  });
+
+  const jsonScreenings = JSON.stringify(filteredScreenings);
+  //console.log(jsonScreenings);
+  return jsonScreenings;
+}
