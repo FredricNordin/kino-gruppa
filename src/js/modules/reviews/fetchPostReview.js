650325728415f19fe8e2f88e5ff36edf70f71c32
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-02-01 15:11:16 +0100

    fetch post node>richard

--- src/js/modules/reviews/fetchPostReview.js
+++ src/js/modules/reviews/fetchPostReview.js
@@ -1,6 +1,23 @@
 import fetch from "node-fetch";
+import { fetchMovieReviews } from "./fetchReviews.js";
 
-
-export default function postReview(data){
-    console.log(data);
-    }
+// Post review sent from the browser.
+export default function postReview(review) {
+  fetch("https://lernia-kino-cms.herokuapp.com/api/reviews/", {
+    method: "POST",
+    mode: "cors",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      data: {
+        author: review.author,
+        comment: review.comment,
+        rating: review.rating,
+        movie: review.movie,
+        verified: review.verified,
+      },
+    }),
+  });
+  // Re:fetch all reviews after post.
+  const movieID = review.movie;
+  return fetchMovieReviews(movieID);
+};
