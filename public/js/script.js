b04a0c5b3b062fb28d6f6502e07cd6764b330620
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-02-01 09:46:52 +0100

    removed script code for merge

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,6 @@
 // LISTA REVIEWS
 // Marcus skriver sitt här
-import changePage from "./func/renderMovieReviews.js";
 
-if (window.location.pathname.match(/\/movies\/[0-9]+/gm)) {
-  changePage();
-}
 
 // SKICKA IN REVIEW
 // Fredric skriver sitt här
