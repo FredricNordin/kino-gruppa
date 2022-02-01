bea872f30b7e3c633cc7e361864fbeb8acb4178b
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-02-01 13:10:05 +0100

    Author verified check on Node

--- src/js/routes/api.js
+++ src/js/routes/api.js
@@ -1,14 +1,28 @@
 import express from "express";
 const apiRouter = express.Router();
 
-
-apiRouter.post('/movies/:movieId/reviews', function (req, res) {
-    console.log(req.body.data)
+// Listen for a post containing review data.
+apiRouter.post("/movies/:movieId/reviews", function (req, res) {
+  // Check if request data fields are filled in correctly to verify the author.
+  if (
+    req.body.data.author != null &&
+    typeof req.body.data.rating === "number" &&
+    typeof req.body.data.movie === "number"
+  ) {
+    const userVerified = true;
+    const data = {
+      author: req.body.data.author,
+      comment: req.body.data.comment,
+      rating: req.body.data.rating,
+      movie: req.body.data.movie,
+      verified: userVerified,
+    };
+    console.log(data);
     res.end();
-})
-
-
-
-
+  } else {
+    console.log("Author not verified. Skipping post!");
+    res.end();
+  }
+});
 
 export default apiRouter;
