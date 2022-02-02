e43597307309884301306676f09f687dc6b6521c
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-02-02 08:08:44 +0100

    Remove console log

--- public/js/modules/fetchScreenings.js
+++ public/js/modules/fetchScreenings.js
@@ -1,7 +1,5 @@
 export async function fetchSingleScreenings(id) {
   const response = await fetch(`/movies/${id}/screenings`);
   const data = response.json();
-  console.log(data);
-
   return data;
 }
