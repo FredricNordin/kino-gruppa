e6cd53ad5fa1b086e9ea98000c2a26399ed9d8ce
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-02-04 19:43:29 +0100

    test removed

--- public/js/script.js
+++ public/js/script.js
@@ -9,21 +9,4 @@ if (window.location.pathname.match(/\/movies\/[0-9]+/gm || window.location.href.
   renderSingleScreenings(movieId[2]);
   changePage();
   createReviewForm();
-  bajskorv();
-};
-
-function bajskorv() {
-  fetch("https://lernia-kino-cms.herokuapp.com/api/reviews/", {
-    method: "POST",
-    mode: "cors",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      data: {
-        author: "Bob",
-        comment: "Very nice.",
-        rating: "5",
-        movie: "1",
-      },
-    }),
-  });
 };
