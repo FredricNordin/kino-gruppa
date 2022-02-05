a17f805d35d2ad47471f4d4f9dc32ff67e7947b7
Ammar Amin <ammar.amin1975@gmail.com>
2022-02-05 21:38:29 +0100

    final cleaning

--- src/js/modules/screenings.js
+++ src/js/modules/screenings.js
@@ -7,12 +7,9 @@ export async function comingScreenings() {
     .filter(obj => {
       const screeningTime = new Date(obj.attributes.start_time);
       return screeningTime > now; // filter condition for screenings of start time greater than now time
-    }).slice(0, 10);
-  // screenings.filter(obj => {
-  //   const screeningTime = new Date(obj.attributes.start_time);
-  //   return screeningTime <= now.setDate(now.getDate() + 5);
-  // });
-  // screenings.slice(0, 20);
+    })
+    .slice(0, 10);
+
 
   // Restructing the filtered screenings
   return {
@@ -23,34 +20,13 @@ export async function comingScreenings() {
         movie: {
           id: obj.attributes.movie.data.id,
           title: obj.attributes.movie.data.attributes.title,
-          image:{
+          image: {
             url: obj.attributes.movie.data.attributes.image.url,
           }
         }
       };
     }),
   };
-  // console.log(screenings);
-  // return screenings;
 }
-
-
-
-// export async function comingScreenings(fetchFunc) {
-//   const today = new Date(); //.toISOString();
-//   /*const now= new Date().getTime();
-//   const fiveDays = now + 86400 * 5;*/
-
-
-//   const obj = await fetchFunc.fetchScreenings(`?populate=movie`); //(`?filters[movie]=${id}`);
-//   const filteredScreenings = [];
-
-//   obj.forEach((obj) => {
-//     if (obj.attributes.start_time > today && obj.attributes.start_time <= today.setDate(today.getDate(today.setHours(-1))+5)) filteredScreenings.push(obj);
-//   });
-
-//   const jsoncomingScreenings = JSON.stringify(filteredScreenings);
-//   return jsoncomingScreenings;
-// }
 
 export default comingScreenings;
