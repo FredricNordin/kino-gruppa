79d6da52f9afd0b4416d115ac2822f0baf637c26
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-02-05 13:15:15 +0100

    Add pagination string

--- src/js/modules/screenings/fetchMovieScreenings.js
+++ src/js/modules/screenings/fetchMovieScreenings.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 export async function fetchScreenings(qs) {
   const response = await fetch(
-    `https://lernia-kino-cms.herokuapp.com/api/screenings${qs}`
+    `https://lernia-kino-cms.herokuapp.com/api/screenings${qs}&pagination[limit]=100`
   );
   const data = await response.json();
   return data.data;
