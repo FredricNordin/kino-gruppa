9d51b3dad4c656217cb196047b11e758bef30e37
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-02-21 15:50:53 +0100

    better structure in sass folder

--- public/js/func/renderNextScreenings.js
+++ public/js/func/renderNextScreenings.js
@@ -3,7 +3,7 @@ import { fetchNextScreenings } from "./fetchScreenings.js";
 export async function renderNextScreenings() {
     let screenings = await fetchNextScreenings();
 
-    document.querySelector("div.main-container").innerHTML = screenings.data.map(screening => `
+    document.querySelector("div.nearest-screenings-container").innerHTML = screenings.data.map(screening => `
         <div class="main-container-content-article-small screening" style="background-image: url('${screening.movie.image.url}');">
             <a title="${screening.movie.title}" href="./movies/${screening.movie.id}"><span>${screening.movie.title}</span><span>${screening.time}</span></a>
         </div>
